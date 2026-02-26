9deaf78acc74c679dc98eedf18a5518522935382
sripriyasrinivas <sripriya.srini04@gmail.com>
2026-02-26 15:40:17 +0530

    Modified http response

--- backend/app.cpp
+++ backend/app.cpp
@@ -41,15 +41,15 @@ int main() {
     while(true) {
         int client_fd = accept(server_fd, NULL, NULL);
         if (client_fd < 0) continue;
-        
-        // Simple HTTP response
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
         std::string response = "HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/plain\r\n";
+        response += "Content-Length: " + std::to_string(body.length()) + "\r\n";
         response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
-        
+        response += body;
         send(client_fd, response.c_str(), response.length(), 0);
         close(client_fd);
+
     }
     
     return 0;
